722d13b7ef01faa102dd62c158d25c545455409e
Svetlana <kuznetsova@66.ru>
2022-10-14 19:40:46 +0500

    fix: исправить ошибки

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,33 @@
 const POPUP_OPENED_CLASS = 'popup_opened';
-const openPopupBtn = document.querySelector('.profile__button_edit');
+const openPopupBtn = document.querySelector('.profile__button-edit');
 const popup = document.querySelector('.popup');
 const popupContent = document.querySelector('.popup__container');
-const closePopupBtn = document.querySelector('.popup__button_close');
+const closePopupBtn = document.querySelector('.popup__button-close');
 
 let formPopup = document.querySelector('.popup__form');
-let nameInput = formPopup.querySelector('.popup__input_name');
-let jobInput = formPopup.querySelector('.popup__input_job');
+let nameInput = formPopup.querySelector('.popup__input_type_name');
+let jobInput = formPopup.querySelector('.popup__input_type_job');
 let profileName = document.querySelector('.profile__title');
 let profileJob = document.querySelector('.profile__paragraph');
 
-openPopupBtn.addEventListener('click', () => {
+let openPopup = function() {
     popup.classList.add(POPUP_OPENED_CLASS);
-})
-
-popup.addEventListener('click', (event) => {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+}
+let closePopup = function(event) {
     if(!popupContent.contains(event.target) || event.target === closePopupBtn) {
         popup.classList.remove(POPUP_OPENED_CLASS);
     }
-})
+}
+openPopupBtn.addEventListener('click', openPopup);
+popup.addEventListener('click', closePopup);
 
-function formSubmitHandler (evt) {
+let formSubmitHandler = function(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
-    popup.classList.remove(POPUP_OPENED_CLASS);
+    closePopup(POPUP_OPENED_CLASS);
 }
 
 formPopup.addEventListener('submit', formSubmitHandler);
