3bee64ea14eabb904c07c677f6bf85f9a339b0c3
Svetlana <kuznetsova@66.ru>
2023-03-25 12:29:04 +0500

    feat: добавить форму обновления аватара, форму согласия удаления карточки

--- src/scripts/components/Card.js
+++ src/scripts/components/Card.js
@@ -1,21 +1,26 @@
 class Card {
-    constructor({data, handleCardClick, handleLikeClick, handleDeleteClick}, currentUserId, templateSelector) {
+    constructor({data, handleCardClick, handleLikeClick, handleDeleteClick, handleDeleteIconClick}, currentUserId, templateSelector) {
         this._data = data;
         this._name = data.name;
         this._link = data.link;
-        this._id = data._id;
-        
+        this._id = data._id;       
         this._likes = data.likes;
         this._currentUserId = currentUserId;
+        this._isOwner = data.owner._id === currentUserId;
         this._handleCardClick = handleCardClick;
         this._handleLikeClick = handleLikeClick;
         this._handleDeleteClick = handleDeleteClick;
+        this._handleDeleteIconClick = handleDeleteIconClick;
         this._templateSelector = templateSelector;
+        this._buttonLikeSelector = '.gallery__button-like';
+        this._buttonDeleteSelector = '.gallery__button-delete';
+        this._buttonLikedClass = 'gallery__button-like_active';
+        this._imageSelector = '.gallery__image';
     }
 
-    _deleteCard() {
+    deleteCard() {
         this._cardElement.remove();
-        this._element = null;
+        this._cardElement = null;
     }
 
     _getTemplate() {
@@ -25,28 +30,30 @@ class Card {
     create() {
         this._cardElement = this._getTemplate();
         this._setEventListeners();
-        this._likeButton = this._cardElement.querySelector('.gallery__button-like');
         this._cardElement.querySelector('.gallery__title').textContent = this._name;
-        this._cardElement.querySelector('.gallery__image').src = this._link;
+        this._cardElement.querySelector(this._imageSelector).src = this._link;
         this._likesCount = this._cardElement.querySelector('.gallery__like-counter');
         this._likesCount.textContent = this._likes.length;
-        this._isLiked();
+        this._toggleLike();
+        this._showDeleteButton();
         
         return this._cardElement;
     }
 
+    _showDeleteButton() {
+        if (!this._isOwner) {
+            this._cardElement.querySelector(this._buttonDeleteSelector).remove();
+        }
+    }
+
     _setEventListeners() {
-        this._cardElement.querySelector('.gallery__button-like').addEventListener('click', () => {
-            if (this._cardElement.classList.contains('gallery__button-like_active')) {
-                this._handleDeleteClick(this);
-              } else {
-                this._handleLikeClick(this);
-              }
+        this._cardElement.querySelector(this._buttonLikeSelector).addEventListener('click', () => {
+            this._handleLikeClick();
         });
-        this._cardElement.querySelector('.gallery__button-delete').addEventListener('click', () => {
-            this._deleteCard();
+        this._cardElement.querySelector(this._buttonDeleteSelector).addEventListener('click', () => {
+            this._handleDeleteIconClick();
         });
-        this._cardElement.querySelector('.gallery__image').addEventListener('click', () => {
+        this._cardElement.querySelector(this._imageSelector).addEventListener('click', () => {
             this._openImage();
         });
     }
@@ -54,23 +61,26 @@ class Card {
         this._handleCardClick(this._name, this._link);
     }
 
-    _isLiked() {
-        if (this._likes.some((user) => {
-            return this._currentUserId === user._id;
-          })) {
-            this._likeButton.classList.add('gallery__button-like_active');
+    isLiked() {
+        return this._likes.some((user) => this._currentUserId === user._id);
+    }
+
+    _toggleLike() {
+        this._likesCount.textContent = this._likes.length;
+        if(this.isLiked()) {
+            this._cardElement.querySelector(this._buttonLikeSelector).classList.add(this._buttonLikedClass);
+          } else {
+            this._cardElement.querySelector(this._buttonLikeSelector).classList.remove(this._buttonLikedClass);
           }
     }
 
-    toggleLike(data) {
+    updateLikes(data) {
         this._likes = data.likes;
-        this._likesCount.textContent = this._likes.length;
-        this._cardElement.querySelector('.gallery__button-like').classList.toggle('gallery__button-like_active');
+        this._toggleLike();
     }
 
-    updateLikes(item) {
-        this._likes = item.likes;
-        this._toggleLike();
+    getId() {
+        return this._id;
     }
 }
 
--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,43 +1,19 @@
-const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-  ];
+const config = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button-save',
+  invalidInputClass: 'popup__input_type_error',
+  disabledSubmitButtonClass: 'popup__button-save_disabled',
+  errorClass: 'popup__input-error_visible'
+};
 
-  const config = {
-    formSelector: '.popup__form',
-    inputSelector: '.popup__input',
-    submitButtonSelector: '.popup__button-save',
-    invalidInputClass: 'popup__input_type_error',
-    disabledSubmitButtonClass: 'popup__button-save_disabled'
-  };
+const profileEditBtn = document.querySelector('.profile__button-edit');
+const profileCardBtn = document.querySelector('.profile__button-add');
+const formEditProfile = document.getElementById('edit-form');
+const editAvatarBtn = document.querySelector('.avatar__button');
+const nameInput = formEditProfile.querySelector('[name="name"]');
+const jobInput = formEditProfile.querySelector('[name="job"]');
+const formAddCard = document.getElementById('add-form');
+const formUpdateAvatar = document.getElementById('update-avatar-form');
 
-  const profileEditBtn = document.querySelector('.profile__button-edit');
-  const profileCardBtn = document.querySelector('.profile__button-add');
-  const formEditProfile = document.getElementById('edit-form');
-  const nameInput = formEditProfile.querySelector('[name="name"]');
-  const jobInput = formEditProfile.querySelector('[name="job"]');
-  const formAddCard = document.getElementById('add-form');
-
-  export { initialCards, config, profileEditBtn, profileCardBtn, formEditProfile, nameInput, jobInput, formAddCard };
+export { config, profileEditBtn, profileCardBtn, formEditProfile, editAvatarBtn, nameInput, jobInput, formAddCard, formUpdateAvatar };
